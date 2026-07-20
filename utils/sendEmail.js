3179a15738cd0c84ff4798a245183d0a63c5a258
MayorK7 <oladejomayowa873@gmail.com>
2026-07-20 09:54:28 +0100

    Fix email service export and SMTP startup crash

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -5,26 +5,6 @@ console.log("EMAIL_HOST =", process.env.EMAIL_HOST);
 console.log("EMAIL_PORT =", process.env.EMAIL_PORT);
 console.log("EMAIL_USER =", process.env.EMAIL_USER);
 
-// const transporter = nodemailer.createTransport({
-
-//     host: process.env.EMAIL_HOST,
-
-//     // port: process.env.EMAIL_PORT,
-//      port: Number(process.env.EMAIL_PORT),
-
-//     secure: true,
-
-//     auth: {
-
-//         user: process.env.EMAIL_USER,
-
-//         pass: process.env.EMAIL_PASS
-
-//     }
-
-// });
-
-
 
 const transporter = nodemailer.createTransport({
 
@@ -51,27 +31,34 @@ const transporter = nodemailer.createTransport({
 });
 
 
-export default transporter;
-
-await transporter.verify();
-console.log("SMTP server is ready");
 
 const sendEmail = async ({ to, subject, html }) => {
 
-    await transporter.sendMail({
+    try {
 
-        from: process.env.EMAIL_FROM,
+        await transporter.sendMail({
 
-        to,
+            from: process.env.EMAIL_USER,
 
-        subject,
+            to,
 
-        html
+            subject,
 
-    });
+            html
+
+        });
+
+        console.log("Email sent to:", to);
+
+    }
+
+    catch(err){
+
+        console.error("Email sending failed:", err.message);
+
+    }
 
 };
-
 
 
 
