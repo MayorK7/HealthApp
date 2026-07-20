348df05fd4ca8a138ab037b3c5467169734075e7
MayorK7 <oladejomayowa873@gmail.com>
2026-07-20 19:58:23 +0100

    making email works again

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,71 +1,28 @@
 import nodemailer from "nodemailer";
 
-
 console.log("EMAIL_HOST =", process.env.EMAIL_HOST);
 console.log("EMAIL_PORT =", process.env.EMAIL_PORT);
 console.log("EMAIL_USER =", process.env.EMAIL_USER);
 
-
 const transporter = nodemailer.createTransport({
-
     host: "smtp.gmail.com",
-
     port: 587,
-
     secure: false,
-
     auth: {
-
         user: process.env.EMAIL_USER,
-
         pass: process.env.EMAIL_PASS
-
     },
-
     tls: {
-
         rejectUnauthorized: false
-
     }
-
 });
 
-
-
-// const sendEmail = async ({ to, subject, html }) => {
-
-//     try {
-
-//         await transporter.sendMail({
-
-//             from: process.env.EMAIL_USER,
-
-//             to,
-
-//             subject,
-
-//             html
-
-//         });
-
-//         console.log("Email sent to:", to);
-
-//     }
-
-//     catch(err){
-
-//         console.error("Email sending failed:", err.message);
-
-//     }
-
-// };
-
-
-
 const sendEmail = async ({ to, subject, html }) => {
+
     console.log("Attempting to send email to:", to);
 
     try {
+
         const info = await transporter.sendMail({
             from: process.env.EMAIL_USER,
             to,
@@ -76,25 +33,11 @@ const sendEmail = async ({ to, subject, html }) => {
         console.log("Email sent successfully:", info.messageId);
 
     } catch (err) {
-        console.error("Full email error:", err);
+
+        console.error("SMTP Error:", err);
+
+        throw err;
     }
 };
-
-
-console.log("sendEmail() called");
-
-try {
-    const info = await transporter.sendMail({
-        from: process.env.EMAIL_USER,
-        to,
-        subject,
-        html
-    });
-
-    console.log("Email sent successfully:", info.messageId);
-
-} catch (err) {
-    console.error("SMTP Error:", err);
-}
 
 export default sendEmail;
